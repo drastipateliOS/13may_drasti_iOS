99b65b38f14a6d51d450dc06b6e09b2e2ed319d1
drastipateliOS <drastipatel1425@gmail.com>
2024-05-13 12:29:49 +0530

    Add files via upload

---
+++ Module-2/Arithmetic_ope.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	//Arithmetic Oper.
+	int a,b;
+	printf("Enter A:");
+	scanf("%d",&a);
+	printf("Enter B:");
+	scanf("%d",&b);
+	printf("\nSum:%d",a+b);
+	printf("\nSub:%d",a-b);
+	printf("\nMul:%d",a*b);
+	printf("\nDiv:%d",a/b);
+	printf("\nMod:%d",a%b);
+	
+}
