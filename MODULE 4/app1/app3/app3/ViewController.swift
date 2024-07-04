40184de607f2509776e123e5b623d046f0055a5b
drastipateliOS <drastipatel1425@gmail.com>
2024-07-04 23:32:50 +0530

    Add files via upload

---
+++ MODULE 4/app1/app3/app3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app3
+//
+//  Created by Drashti Patel on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var txt: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btn_display(_ sender: Any) 
+    {
+        txt.text=lbl.text
+    }
+}
+
---
+++ MODULE 4/app1/app5/app5/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  app5
+//
+//  Created by Drashti Patel on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        background.image = UIImage(named: "3.png")
+    }
+
+
+}
+
